5212987357b148f154d6d6095089c41810f0a0a5
Youen <youenlebihan29@gmail.com>
2024-10-07 15:15:38 +0200

    feat : add function to verify if content is present in an html element

--- PageVerifier.js
+++ PageVerifier.js
@@ -24,6 +24,18 @@ class PageVerifier
     {
         await this.page.click(selector);
     }
+    async contentIsPresent(selector, content)
+    {
+        //Attendre que l'élément soit chargé
+        await this.page.waitForSelector(selector);
+        //Récupérer l'élément
+        const element = await this.page.$(selector);
+        //Récupérer le contenu de l'élément
+        const elementContent = await this.page.evaluate(element => element.textContent, element);
+        //Vérifie que l'élément est présent
+        const isPresent = elementContent.includes(content);
+        return isPresent;
+    }
     async closePage()
     {
         await this.page.close();
